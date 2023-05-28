694718de0e077027da45f42b88ea306a14504c1e
sergiu-sotoc <sergiu.sotoc@softvision.com>
2023-05-28 23:06:26 +0300

    added search by id

--- car_information_viewer/main.py
+++ car_information_viewer/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, Path, HTTPException, status, Body, Request
+from fastapi import FastAPI, Query, Path, HTTPException, status, Body, Request, Form
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
@@ -41,9 +41,7 @@ def root(request: Request):
 def get_cars(request: Request, number: Optional[str] = Query("10",max_length=3)):
     response = []
     for id, car in list(cars.items())[:int(number)]:
-        to_add = {}
-        to_add[id] = car
-        response.append(to_add)
+        response.append((id,car))
     return templates.TemplateResponse("index.html", {"request": request, "cars": response, "title": "Home"})
 
 @app.get("/cars", response_model=List[Dict[str, Car]])
@@ -55,12 +53,23 @@ def get_cars(number: Optional[str] = Query("10",max_length=3)):
         response.append(to_add)
     return response
 
-@app.get("/cars/{id}", response_model=Car)
-def get_car_by_id(id: int = Path(...,ge=0,lt=1000)):
+@app.post("/search", response_class=RedirectResponse)
+def search_cars(id: str = Form(...)):
+    return RedirectResponse(url="/cars/" + id, status_code=302)
+
+# @app.get("/cars/{id}", response_model=Car)
+# def get_car_by_id(id: int = Path(...,ge=0,lt=1000)):
+#     car = cars.get(id)
+#     if not car:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could not find car by ID")
+#     return car
+@app.get("/cars/{id}", response_class=HTMLResponse)
+def get_car_by_id(request: Request, id: int = Path(...,ge=0,lt=1000)):
     car = cars.get(id)
+    response = templates.TemplateResponse("search.html", {"request": request, "car": car, "id": id, "title": "Search Car"})
     if not car:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could not find car by ID")
-    return car
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return response
 
 @app.post("/cars", status_code=status.HTTP_201_CREATED)
 def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
