156bb4969e28c220f7ef2733da40d046bfb22e7d
sergiu-sotoc <sergiu.sotoc@softvision.com>
2023-05-30 22:24:23 +0300

    added welcome page in social media feed project

---
+++ social_media_feed/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from typing import List, Optional
+from db import users
+
+class Notification(BaseModel):
+    author: str
+    description: str
+
+class User(BaseModel):
+    name: str
+    username: str
+    email: str
+    birthday: str
+    friends: List[str]
+    notifications: List[Notification]
+
+class UserDB(User):
+    hashed_password: str
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "FriendConnect - Home"})
